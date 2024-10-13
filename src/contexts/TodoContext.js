76c0ba5a106adffecd9b8af7210f49d5dc5913fb
Bilal <memonbilal760@gmail.com>
2024-10-13 17:17:58 +0500

    Completion of todo-app with context api

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+export const TodoContext =  createContext({
+    todos: [
+        
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
